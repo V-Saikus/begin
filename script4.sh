4afe6b36b2b0f07de04711dd8aeb37d610d785a0
root <root@localhost.localdomain>
2018-09-27 09:35:38 +0300

    Script4 is normal now

--- script4.sh
+++ script4.sh
@@ -2,16 +2,23 @@
 #^
 url="https://postcode.in.ua/ua/codes/city/1007/a"
 
-#curl $url | grep -i "text_tab_1"| grep -oE "https://postcode.in.ua/ua/codes/city/1007/street/[0-9]+" > adresses
+curl $url | grep -i "text_tab_1"| grep -oE "https://postcode.in.ua/ua/codes/city/1007/street/[0-9]+" > adresses
+
+while read line
+do
+curl $line | grep -oE "https://postcode.in.ua/images/image_c.php\?id=[0-9]+" >> adressespng
+done < adresses
+
+while read line
+do
+wget -P /root/reps/begin/images_script4/ "$line"
+done < adressespng
+
 count=0
 p=.png
-png="`ls /root/reps/begin/`"
+png="`ls /root/reps/begin/images_script4`"
 for line in $png;
 do
-#echo -E "$line"
-#wget -P /root/reps/begin/images_script4/ `
-#curl $line | grep -oE "https://postcode.in.ua/images/image_c.php\?id=[0-9]+" >> adressespng
-
 count=$((count+1))
 
 mv /root/reps/begin/images_script4/"$line" images_script4/"IMG$count$p"
